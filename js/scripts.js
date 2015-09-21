6a0ee07c3399753e20c75fe3ef8272a4c2459065
nataliyamiller <nataliyamiller82@gmail.com>
2015-09-21 13:49:25 -0700

    Adds css3 file

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
+$(document).ready(function() {
+  $("form#new-task").submit(function(event) {
+    event.preventDefault();
 
+    var inputtedTaskDescription = $("input#new-task-description").val();
+    var inputtedTaskImportance = $("input#new-task-importance").val();
+
+    var newTask = { description: inputtedTaskDescription, importance: inputtedTaskImportance };
+
+    $("ul#tasks-to-do").append("<li><span class='to-do'>" + newTask.description + " " + newTask.importance + "</span></li>");
+
+    $("input#new-task-description").val("");
+    $("input#new-task-importance").val("");
+  });
+});
