2d7f93a66e1208190692e2139db3b2aec54db1e1
nataliyamiller <nataliyamiller82@gmail.com>
2015-09-21 19:46:47 -0700

    Adds moving button, but still needs fixing

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,45 @@ $(document).ready(function() {
     var inputtedTaskImportance = $("input#new-task-importance").val();
 
 
-    var newTask = { description: inputtedTaskDescription, importance: inputtedTaskImportance };
+    var newTask = { description: inputtedTaskDescription, importance: inputtedTaskImportance, item: [] };
 
-    $("ul#tasks-to-do").append("<li><span class='to-do'>" + newTask.description + " </span><button id='move'> Move to completed tasks</button></li>");
-    $("button#move").click(function(){
-    $(".to-do").detach().prependTo("ul#tasks-completed");
-    $("button").detach();
-    $("li").remove();
-  });
+  var item = "<li><span class='to-do'>" + newTask.description + "<button id='move'>Move</button></span></li>";
+
+   $("ul#tasks-to-do").append(item);
+
+
+
+   var clickedButton = $(".to-do #move").last().click(function() {
+     alert("button clicked");
+     var $this = $(this);
+     $this.toggleClass("move");
+     if($this.hasClass("move")) {
+       $this.text("Move to to-do");
+     } else {
+       $this.text("Move to completed");
+     }
+     var $toDo = $(".to-do");
+     $toDo.toggleClass(".to-do");
+     if($toDo.hasClass(".to-do")) {
+     $toDo.detach().appendTo("ul#tasks-completed");
+   } else {
+     $toDo.detach().appendTo("ul#tasks-to-do");
+   }
+   });
+
+
+
+ //   $(".item").click(function() {
+ //     $("button#move").click(function() {
+ //     $(".item").detach().appendTo("ul#tasks-completed");
+ //     $(this).removeClass("move").addClass("moved").appendTo("ul#tasks-completed");
+ //   });
+ // });
+
+  //  $("button#move").last().click(function(){
+  //   $(".to-do").detach().prependTo("ul#tasks-completed");
+  //   $(this).removeClass("move").addClass("moved").appendTo("ul#tasks-completed");
+  // });
 
     $("input#new-task-description").val("");
     $("input#new-task-importance").val("");
@@ -24,6 +55,6 @@ $(document).ready(function() {
     $("#show-task-to-do h2").text(newTask.description);
     $(".task-description").text(newTask.description);
     $(".task-importance").text(newTask.importance);
+      });
     });
   });
-});
